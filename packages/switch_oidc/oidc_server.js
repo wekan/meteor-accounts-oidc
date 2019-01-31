800a77f71e10a773fc41b2c4009c0118c3eccb3b
root de plmkan.bordeaux <root@plmkan.bordeaux>
2019-01-31 15:57:02 +0100

    debug

--- packages/switch_oidc/oidc_server.js
+++ packages/switch_oidc/oidc_server.js
@@ -88,7 +88,7 @@ var getUserInfo = function (accessToken) {
   var config = getConfiguration();
   // Some userinfo endpoints use a different base URL than the authorization or token endpoints.
   // This logic allows the end user to override the setting by providing the full URL to userinfo in their config.
-  if config.userinfoEndpoint.includes("https://") {
+  if (config.userinfoEndpoint.includes("https://")) {
     var serverUserinfoEndpoint = config.userinfoEndpoint;
   } else {
     var serverUserinfoEndpoint = config.serverUrl + config.userinfoEndpoint;
